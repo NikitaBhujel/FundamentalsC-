57e32a2731ab448380f7ef05c6c96d32112dcfe8
Nikita Bhujel <nikibhujel1@gmail.com>
2025-08-20 22:28:07 +0545

    console.writeline modified

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -7,6 +7,7 @@ namespace fundamental
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets get started");
         }
     }
 }
